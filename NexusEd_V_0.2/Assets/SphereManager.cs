6f658e343abc95be55c5b098407122fb8323f079
LiamBilbie <connorsburgess@gmail.com>
2023-04-04 20:27:50 -0400

    Finished Geo Game (Peer Programming session 3 for the day)
    Finalized sequence of Geography game as well as made some QOL fixes.

--- NexusEd_V_0.2/Assets/SphereManager.cs
+++ NexusEd_V_0.2/Assets/SphereManager.cs
@@ -9,6 +9,9 @@ public class SphereManager : MonoBehaviour
 
     public GameObject mySphere;
 
+    private bool colStatus = false;
+    List<string> collidingNations = new List<string>();
+
     public GameObject myEarth;
     // Start is called before the first frame update
     void Start()
@@ -24,17 +27,35 @@ public class SphereManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "EarthCollider")
+        //Export the thing we're colliding with to the list
+        collidingNations.Add(other.gameObject.tag);
+        //Light functionality if it's the earth we're colliding with for UI
+        if (other.tag == "EarthCollider")
         {
+            colStatus = true;
             mySphere.GetComponent<MeshRenderer>().material = emissive;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        //Get rid of the game object we're no longer colliding with
+        collidingNations.Remove(other.gameObject.tag);
+        //Light functionality if it's the earth we're colliding with for UI
         if(other.tag == "EarthCollider")
         {
+            colStatus = false;
             mySphere.GetComponent<MeshRenderer>().material = holoGreen;
         }
+    }
+
+    public bool GetColStatus()
+    {
+        return colStatus;
+    }
+
+    public List<string> getCurrentNations()
+    {
+        return collidingNations;
     }
 }
--- NexusEd_V_0.2/Assets/navGameManager.cs
+++ NexusEd_V_0.2/Assets/navGameManager.cs
@@ -3,42 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class navGameManager : MonoBehaviour
 {
     public GameObject holoSphere;
     public GameObject holoEarth;
     public GameObject tutText;
+    public SphereManager mySphereManager;
+    public GameObject scoreboard;
+    public GameObject winboard;
+    public GameObject ExitTrigger;
 
+
+    public XRRayInteractor myRight;
+    public XRRayInteractor myLeft;
+ 
     public InputActionReference a_Button = null, lGrip = null, b_button = null, x_Button = null;
 
     private int[] state = new int[2];
+    private int myIndex;
+    private int score;
+    private int questions;
+    private TMP_Text scoreText;
+    private BoxCollider exitCollider;
 
     private string[] regions = { "Africa", "South America", "Central America", "North America", "The Arctic", "Europe", "Asia", 
         "The Middle East", "Oceania", "The Atlantic Ocean", "The Pacific Ocean", "The Mediterranean Sea", "The Indian Ocean", "The Caribbean"};
+    private string[] myTags = { "Africa", "SouthAmerica", "CentralAmerica", "NorthAmerica", "Arctic", "Europe", "Asia", 
+        "MiddleEast", "Oceania", "AtlanticOcean", "PacificOcean", "MediterraneanSea", "IndianOcean", "Caribbean"};
 
     // Start is called before the first frame update
     void Start()
     {
         state[0] = 0;
         state[1] = 0;
+
+        mySphereManager = holoSphere.GetComponent<SphereManager>();
+        score = 0;
+        questions = 0;
+        scoreText = scoreboard.GetComponentInChildren<TMP_Text>();
+
+        exitCollider = ExitTrigger.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Change tutorial text and other elements on state changes
-       if(state[0] == 0 && state[1] == 1)
+        //First check is to toggle tutorial text (we can do that after we've gone past the welcome message)
+        if(x_Button.action.triggered && state[1] > 1)
         {
-
+            tutText.SetActive(!tutText.activeSelf);
         }
-       else if(state[0] == 1 && state[1] == 2)
-        {
 
-        }
-       else if(state[0] == 2 && state[1] == 3)
+        //Once we've scored more than five, we can let the user know they've calibrated the machine
+        if(score >= 5)
         {
-
+            winboard.SetActive(true);
+            exitCollider.enabled = true;
         }
 
        //Take actions dependent on current state and current state alone (i.e. listening for submit inputs, showing feedback text, etc.)
@@ -47,8 +70,65 @@ public class navGameManager : MonoBehaviour
             if (a_Button.action.triggered)
             {
                 state[1] = 1;
-                tutText.GetComponentInChildren<TMP_Text>().text = "You can grab the holo-globe to rotate it and place the holo-cube inisde. \n (press A to continue)";
+                tutText.GetComponentInChildren<TMP_Text>().text = "You can grab the holo-globe to rotate it and place the holo-cube inisde. Give it a try! \n (toggle this tutorial text with X)";
+                myLeft.selectEntered.AddListener(ShowInstructions);
+                myRight.selectEntered.AddListener(ShowInstructions);
             }
         }
+       else if (state[1] == 2)
+        {
+            if (a_Button.action.triggered)
+            {
+                state[1] = 3;
+                ShowCountry();
+            }
+        }
+       else if (state[1] == 3)
+        {
+            if (a_Button.action.triggered)
+            {
+                //If there's an intersection, we can check that it's the correct one
+                if (mySphereManager.GetColStatus())
+                {
+                    //If the intersection is correct, we can give the user more score and move on
+                    if (mySphereManager.getCurrentNations().Contains(myTags[myIndex]))
+                    {
+                        score++;
+                        questions++;
+                        scoreText.text = "Score: " + score.ToString() + "/" + questions.ToString();
+                        ShowCountry();
+                    }
+                    else
+                    {
+                        questions++;
+                        scoreText.text = "Score: " + score.ToString() + "/" + questions.ToString();
+                        ShowCountry();
+                    }
+                }
+                //If there's no intersection, we need to prompt the user to intersect properly
+                else
+                {
+                    tutText.GetComponentInChildren<TMP_Text>().text = "Please find: " + regions[myIndex] + 
+                        "\n (Press A to lock in your answer)" + "\n Make sure the sphere is glowing white!";
+                }
+            }
+        }
+    }
+
+    private void ShowInstructions(SelectEnterEventArgs arg0)
+    {
+        if(state[1] == 1)
+        {
+            tutText.GetComponentInChildren<TMP_Text>().text = "Well done! We need a navigator to re-calibrate the system. We'll ask you to place the " +
+                "holo-sphere inside some geographic areas, and if you get us enough correct references, we should be able to get back up and running" +
+                "\n (press A to start the calibration process)";
+            state[1] = 2;
+        }
+    }
+
+    private void ShowCountry()
+    {
+        myIndex = Random.Range(0, regions.Length);
+        tutText.GetComponentInChildren<TMP_Text>().text = "Please find: " + regions[myIndex] + "\n (Press A to lock in your answer)";
     }
 }
